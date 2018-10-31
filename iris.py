1b941cca0a4275e8993819b3e38b6183e3577127
marcustorm <marcus.storm@outlook.com>
2018-10-31 09:30:59 +0000

    wrote import data functions

--- iris.py
+++ iris.py
@@ -1,6 +1,7 @@
 import numpy
 import pandas
 import nltk
+from matplotlib.pyplot import plot as plt
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 
@@ -11,5 +12,14 @@ names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
 
+print(dataset.shape)
+
 pandas.set_option('display.max_columns', 10)
-print(dataset.head())
+print(dataset.head(20))
+
+print(dataset.describe())
+
+print(dataset.groupby('class').size())
+
+plot = dataset.plot(kind='box')
+plt.show()
