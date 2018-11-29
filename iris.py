3ef285b93e62fecbbbaaf0012256698f54cf4610
marcustorm <marcus.storm@outlook.com>
2018-11-29 19:15:44 +0000

    changed to main.py

--- iris.py
+++ iris.py
@@ -6,8 +6,22 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import sklearn
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
+
+# import the data from the web
 dataset = pandas.read_csv(url, names=names)
 
-print(dataset.shape)
+# what are the dimensions of the data?
+# print("Dimensions of data:", dataset.shape)
+
+# make sure print all the columns
+# print the first 10 rows of the data
+pandas.set_option('display.width', 1000)
+pandas.set_option('display.max_columns', 500)
+# print(dataset.head(10))
+
+# print(dataset.tail(10))
+
+# print(dataset.describe())
+
