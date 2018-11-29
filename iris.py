9b169763b5a624d4455cef573366e1ba0e1da404
marcustorm <marcus.storm@outlook.com>
2018-11-29 19:41:02 +0000

    changed to main.py

--- iris.py
+++ iris.py
@@ -5,6 +5,16 @@ import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
 
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
@@ -40,3 +50,35 @@ pandas.set_option('display.max_columns', 500)
 # multivariate plot
 # scatter_matrix(dataset)
 # plt.show()
+
+array = dataset.values
+X = array[:, 0:4]
+Y = array[:, 4]
+
+validation_size = 0.2
+seed = 7
+X_train, X_validation, Y_train, Y_validation = sklearn.model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                        random_state=seed)
+scoring="accuracy"
+
+
+# Check algorithms
+
+models = []
+models.append(("LR", LogisticRegression()))
+models.append(("LDA", LinearDiscriminantAnalysis()))
+models.append(("KNN", KNeighborsClassifier()))
+models.append(("CART", DecisionTreeClassifier()))
+models.append(("NB", GaussianNB()))
+models.append(("SVM", SVC()))
+
+# evaluate models
+results = []
+names = []
+for name, model in models:
+    kfold = sklearn.model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = sklearn.model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f(%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
