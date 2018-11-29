3a0043a5c7b009b6547ae31b52f3b052041ae9dd
marcustorm <marcus.storm@outlook.com>
2018-11-29 18:54:32 +0000

    changed to main.py

--- iris.py
+++ iris.py
@@ -1,6 +1,13 @@
-import numpy
-import scipy
-import matplotlib
+# Github link
+# https://github.com/marcustorm/kclml/blob/master/iris.py
+
 import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
 import sklearn
 
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
+dataset = pandas.read_csv(url, names=names)
+
+print(dataset.shape)
